d8ff90d12ed91c403dd1d88d7f189022c9dc5fd7
Rebekah Banks <168665115+Banksrm75@users.noreply.github.com>
2024-10-12 00:34:00 +0000

    Basic programming for all components complete

--- src/js/component/CardRow.jsx
+++ src/js/component/CardRow.jsx
@@ -1,11 +1,15 @@
-
 import React from 'react';
+import Card from './Card.jsx'
 
 const CardRow = () => {
 
     return (
-        <>
-        
+        <> /// fix so that when squished, they go back to one on top of each other
+            <div className="d-flex">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </>
 
     );
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import CardRow from "./CardRow.jsx";
+import Footer from "./Footer.jsx";
+
 
 
 //create your first component
 const Home = () => {
 	return (
 		<>
-		
+			<Navbar />
+
+			<div className="row">
+				<div className="col-2"></div>
+				<div className="col-8">
+					<Jumbotron />
+				</div>
+				<div className="col-2"></div>
+			</div>
+			
+			<div className="row">
+				<div className="col-4"></div>
+				<div className="col-4">
+					<CardRow />
+				</div>
+				<div className="col-4"></div>
+			</div>
+
+			<Footer />
+
 		</>
 	);
 };
